dcea029c537b6fa8274e5ab13df9a32e8c500fad
reillykeele <54957139+reillykeele@users.noreply.github.com>
2023-04-01 22:28:23 -0500

    Add Object Pool Wrapper

--- Tanks/Assets/Scripts/Util/Pooling/BasePool.cs
+++ Tanks/Assets/Scripts/Util/Pooling/BasePool.cs
@@ -1,33 +1,46 @@
+using System;
 using UnityEngine;
-using UnityEngine.Rendering;
+using UnityEngine.Pool;
 
-namespace Util.Pooling
+/// <summary>
+/// A base object pool wrapper providing generic event implementations.
+/// 
+/// Derive from this class, call InitPool and use Get and Release.
+/// </summary>
+/// <typeparam name="T">MonoBehaviour object to pool.</typeparam>
+public abstract class BasePool<T> : MonoBehaviour where T : MonoBehaviour
 {
-    public abstract class BasePool<T> : MonoBehaviour where T : MonoBehaviour
+    private T _prefab;
+    
+    private ObjectPool<T> _pool;
+    private ObjectPool<T> Pool
     {
-        // TODO: Update to newer Unity version
-        // private T _prefab;
-        // private ObjectPool<T> _pool;
-        //
-        // protected void InitPool(T prefab, int initial = 10, int max = 20, bool collectionChecks = false)
-        // {
-        //     _prefab = prefab;
-        //     _pool = new ObjectPool<T>(
-        //         CreateSetup,
-        //         GetSetup,
-        //         ReleaseSetup,
-        //         DestroySetup,
-        //         collectionChecks,
-        //         initial,
-        //         max);
-        // }
-        //
-        // protected virtual T CreateSetup() => Instantiate(_prefab);
-        // protected virtual void GetSetup(T obj) => obj.gameObject.SetActive(true);
-        // protected virtual void ReleaseSetup(T obj) => obj.gameObject.SetActive(false);
-        // protected virtual void DestroySetup(T obj) => Destroy(obj);
-        //
-        // public T Get() => _pool.Get();
-        // public void Release(T obj) => _pool.Release(obj);
+        get
+        {
+            if (_pool == null) throw new InvalidOperationException("InitPool must be called before using the ObjectPool.");
+            return _pool;
+        }
+        set => _pool = value;
     }
+
+    protected void InitPool(T prefab, int initial = 10, int max = 20, bool collectionChecks = false)
+    {
+        _prefab = prefab;
+        Pool = new ObjectPool<T>(
+            CreateSetup,
+            GetSetup,
+            ReleaseSetup,
+            DestroySetup,
+            collectionChecks,
+            initial,
+            max);
+    }
+
+    protected virtual T CreateSetup() => Instantiate(_prefab);
+    protected virtual void GetSetup(T obj) => obj.gameObject.SetActive(true);
+    protected virtual void ReleaseSetup(T obj) => obj.gameObject.SetActive(false);
+    protected virtual void DestroySetup(T obj) => Destroy(obj);
+
+    public T Get() => Pool.Get();
+    public void Release(T obj) => Pool.Release(obj);
 }
